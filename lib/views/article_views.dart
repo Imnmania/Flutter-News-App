cafda5b251a19308cd81aa21b36a31ef29325e7d
Imnmania <niloyb@live.com>
2021-02-08 03:16:46 +0600

    article view working

--- lib/views/article_views.dart
+++ lib/views/article_views.dart
@@ -1,13 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleView extends StatefulWidget {
+  //
+  final String blogUrl;
+  ArticleView({@required this.blogUrl});
+
   @override
   _ArticleViewState createState() => _ArticleViewState();
 }
 
 class _ArticleViewState extends State<ArticleView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Flutter'),
+            Text(
+              'News',
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          Opacity(
+            opacity: 0.0,
+            child: Container(
+              margin: EdgeInsets.only(right: 10),
+              child: IconButton(
+                icon: Icon(Icons.save),
+                onPressed: null,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: (WebViewController webViewController) {
+            _completer.complete(webViewController);
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/blog_tile.dart
+++ lib/widgets/blog_tile.dart
@@ -1,49 +1,64 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/views/article_views.dart';
 
 class BlogTile extends StatelessWidget {
   final String imageUrl;
   final String title;
   final String description;
+  final String url;
 
   BlogTile({
     @required this.imageUrl,
     @required this.title,
     @required this.description,
+    @required this.url,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 10, bottom: 10),
-      child: Column(
-        children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: CachedNetworkImage(imageUrl: imageUrl)),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.blue[900],
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        child: Column(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: CachedNetworkImage(imageUrl: imageUrl)),
+            SizedBox(
+              height: 5,
             ),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            description,
-            style: TextStyle(
-              color: Colors.black54,
-              fontWeight: FontWeight.bold,
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.blue[900],
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                color: Colors.black54,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ArticleView(
+              blogUrl: url,
+            ),
+          ),
+        );
+      },
     );
   }
 }
